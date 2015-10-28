679aced0c2a6ec481c1b3f3dcc1d8f1824e2f90f
Hunter Cassidy <hunter@mobelux.com>
2015-10-28 11:30:18 -0400

    add todo test case

--- reducers/todos.js
+++ reducers/todos.js
@@ -5,7 +5,7 @@ export default function todos(state = [], action) {
 	switch (action.type) {
 	case ADD_TODO:
 		return [{
-			id: uuid.v4(),
+			id: state.reduce((maxId, todo) =>  Math.max(todo.id, maxId), -1) + 1,
 			completed: false,
 			text: action.text
 		}, ...state];
